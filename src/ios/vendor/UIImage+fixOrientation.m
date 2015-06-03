e1bbd2409a357b2674b82cef5cfc61f22fe8bce0
KshinG <github@valis.ca>
2015-06-02 21:33:00 -0700

    test 3rd crop

--- src/ios/vendor/UIImage+fixOrientation.m
+++ src/ios/vendor/UIImage+fixOrientation.m
@@ -23,13 +23,13 @@
         case UIImageOrientationDown:
         case UIImageOrientationDownMirrored:
             //phone top left selfie landscape
-            cropRect = CGRectMake( self.size.width*0.2, 0, self.size.width*0.8, 0.8*self.size.height);
+            cropRect = CGRectMake( self.size.width*0.1, 0, self.size.width*0.8, 0.8*self.size.height);
             break;
 
         case UIImageOrientationLeft:
         case UIImageOrientationLeftMirrored:
-            //phone top down
-            cropRect = CGRectMake( 138, 138, self.size.height*0.8, self.size.width*0.8);
+            //phone top down selfie portrait
+            cropRect = CGRectMake( 138, 138, self.size.height*0.2, self.size.width*0.2);
             break;
 
         case UIImageOrientationRight:
@@ -40,7 +40,7 @@
         case UIImageOrientationUp:
         case UIImageOrientationUpMirrored:
             //phone top right back landscape
-            cropRect = CGRectMake( 138, 0, self.size.width*0.2, self.size.height*0.2);
+            cropRect = CGRectMake( self.size.width*0.1, 0, self.size.width*0.8, self.size.height*0.8);
 
             break;
     }
